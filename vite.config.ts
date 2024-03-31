8badd7f62611ac31ab2230e491233f31d15b7f39
Yolosopher <yolosopher@gmail.com>
2024-03-31 15:34:39 +0400

    mkcert removed

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import path from "path";
 import { defineConfig } from "vite";
-import mkcert from "vite-plugin-mkcert";
+// import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [mkcert()],
+  plugins: [],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
