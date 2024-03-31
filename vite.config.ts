be6214f0687c49adab20226c9f1a24e7bb288173
Yolosopher <yolosopher@gmail.com>
2024-03-31 15:41:44 +0400

    x

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import path from "path";
 import { defineConfig } from "vite";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [],
+  plugins: [mkcert()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
